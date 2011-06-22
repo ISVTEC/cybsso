671880668e73e4586025878c9086c96908fa013d
cyb <cyb@63a8e5df-aab3-48d7-8c8d-b200f9e8f67e>
2011-06-22 17:58:50 +0000

    Add
    git-svn-id: svn+ssh://svn.isvtec.com/var/lib/subversion/admin/cybsso@2078 63a8e5df-aab3-48d7-8c8d-b200f9e8f67e

---
+++ template-app/index.php
@@ -0,0 +1,62 @@
+<?php
+
+session_start();
+
+# Check if ticket is defined and is still valid
+if(!isset($_SESSION['ticket'],
+		  $_SESSION['ticket_expiration_date'],
+		  $_SESSION['cybsso_user']['email']) or
+   $_SESSION['ticket_expiration_date'] <= time()) {
+
+	try{
+		$return_url = (($_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://') .
+			$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+		# Connect to the SSO API
+		$sso_param = array(
+			// 'location' => 'https://login.isvtec.com/',
+			'location' => 'http://cybsso-api.dev.isvtec.com/',
+			'login'    => 'api-login',
+			'password' => 'api-password',
+			'uri'      => '',
+			);
+
+		$cybsso = new SoapClient(null, $sso_param);
+
+		# Redirect to the auth page if ticket is invalid and no information is
+		# given
+		if(!isset($_GET['cybsso_ticket'], $_GET['cybsso_email'])) {
+			header('Location: ' . $cybsso->url() . "?return_url=$return_url");
+			exit;
+		}
+
+		# If the user has just logged in, then we set the session and redirect
+		# to ourself
+		$expiration = $cybsso->TicketCheck($_GET['cybsso_ticket'],
+										   $_GET['cybsso_email']);
+
+		$cybsso_user = $cybsso->UserGetInfo($_GET['cybsso_email']);
+
+		$_SESSION = array(
+			'ticket'                 => $_GET['cybsso_ticket'],
+			'ticket_expiration_date' => $expiration,
+			'cybsso_user'            => $cybsso_user,
+			'cybsso_url'             => $cybsso->url(),
+		);
+
+		header("Location: $return_url");
+		exit;
+	}
+	catch(SoapFault $fault) {
+		# Ticket is invalid, go back to the SSO
+		header('Location: ' . $cybsso->url() . "?return_url=$return_url");
+		exit;
+	}
+}
+
+echo '<pre>';
+print_r($_SESSION['cybsso_user']);
+
+?>
+<br/>
+<a href="<?=$_SESSION['cybsso_url']?>?action=logout">Logout</a>
