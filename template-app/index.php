d6a6d093ea66afbf0166f434d9b2f0ca0038d3c2
cyb <cyb@63a8e5df-aab3-48d7-8c8d-b200f9e8f67e>
2012-06-25 12:09:34 +0000

    template-app/index.php: fixed indentation
    git-svn-id: svn+ssh://svn.isvtec.com/var/lib/subversion/admin/cybsso@3525 63a8e5df-aab3-48d7-8c8d-b200f9e8f67e

--- template-app/index.php
+++ template-app/index.php
@@ -3,64 +3,64 @@
 session_start();
 
 try{
-	# Connect to the SSO API
-	define('CYBSSO_URL', 'https://login.isvtec.com/');
-	$cybsso = new SoapClient(null, array(
-								 'location' => CYBSSO_URL . 'api/',
-								 'login'    => 'api-login',
-								 'password' => 'api-password',
-								 'uri'      => '',
-								 ));
+  // Connect to the SSO API
+  define('CYBSSO_URL', 'https://login.isvtec.com/');
+  $cybsso = new SoapClient(null, array(
+              'location' => CYBSSO_URL . 'api/',
+              'login'    => 'api-login',
+              'password' => 'api-password',
+              'uri'      => '',
+            ));
 
-	$return_url = (($_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://') .
-		$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  $return_url = (($_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://') .
+    $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-	# Check if ticket is defined and is still valid
-	if(!isset($_SESSION['cybsso_ticket'],
-			  $_SESSION['cybsso_ticket_expiration_date'],
-			  $_SESSION['cybsso_user']['email']) or
-	   $_SESSION['cybsso_ticket_expiration_date'] <= time()) {
+  // Check if ticket is defined and is still valid
+  if(!isset($_SESSION['cybsso_ticket'],
+      $_SESSION['cybsso_ticket_expiration_date'],
+      $_SESSION['cybsso_user']['email']) or
+    $_SESSION['cybsso_ticket_expiration_date'] <= time()) {
 
-		# Redirect to the auth page if ticket is invalid and no information is
-		# given
-		if(!isset($_GET['cybsso_ticket'], $_GET['cybsso_email']))
-			throw new SoapFault('Invalid SSO ticket');
+    // Redirect to the auth page if ticket is invalid and no information is
+    // given
+    if(!isset($_GET['cybsso_ticket'], $_GET['cybsso_email']))
+      throw new SoapFault('index.php', 'Invalid SSO ticket');
 
-		# If the user has just logged in, then we set the session and redirect
-		# to ourself
-		$expiration = $cybsso->TicketCheck($_GET['cybsso_ticket'],
-										   $_GET['cybsso_email']);
+    // If the user has just logged in, then we set the session and redirect
+    // to ourself
+    $expiration = $cybsso->TicketCheck($_GET['cybsso_ticket'],
+                  $_GET['cybsso_email']);
 
-		$cybsso_user = $cybsso->UserGetInfo($_GET['cybsso_email']);
+    $cybsso_user = $cybsso->UserGetInfo($_GET['cybsso_email']);
 
-		$_SESSION = array(
-			'cybsso_ticket'                 => $_GET['cybsso_ticket'],
-			'cybsso_ticket_expiration_date' => $expiration,
-			'cybsso_user'                   => $cybsso_user,
-		);
+    $_SESSION = array(
+      'cybsso_ticket'                 => $_GET['cybsso_ticket'],
+      'cybsso_ticket_expiration_date' => $expiration,
+      'cybsso_user'                   => $cybsso_user,
+    );
 
-		header("Location: $return_url");
-		exit;
-	}
+    header("Location: $return_url");
+    exit;
+  }
 
-	# Check if the ticket is valid
-	$_SESSION['cybsso_ticket_expiration_date'] =
-		$cybsso->TicketCheck($_SESSION['cybsso_ticket'],
-							 $_SESSION['cybsso_user']['email']);
-	unset($return_url);
+  // Check if the ticket is valid
+  $_SESSION['cybsso_ticket_expiration_date'] =
+    $cybsso->TicketCheck($_SESSION['cybsso_ticket'],
+      $_SESSION['cybsso_user']['email']);
+  unset($return_url);
 }
 catch(SoapFault $fault) {
-	# If the ticket is invalid for some reason, then we destroy the session and
-	# redirect to the SSO
-	$_SESSION = array();
-	if (ini_get('session.use_cookies')) {
-		$params = session_get_cookie_params();
-		setcookie(session_name(), '', time() - 42000, $params['path'],
-				  $params['domain'], $params['secure'], $params['httponly']);
-	}
-	session_destroy();
-	header('Location: ' . CYBSSO_URL . "?return_url=$return_url");
-	exit;
+  // If the ticket is invalid for some reason, then we destroy the session and
+  // redirect to the SSO
+  $_SESSION = array();
+  if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'],
+      $params['domain'], $params['secure'], $params['httponly']);
+  }
+  session_destroy();
+  header('Location: ' . CYBSSO_URL . "?return_url=$return_url");
+  exit;
 }
 
 echo '<pre>';
@@ -69,4 +69,4 @@ print_r($_SESSION['cybsso_user']);
 </pre>
 <br/>
 <a href="logout.php">Logout</a> <br/>
-<a href="<?=CYBSSO_URL?>/self/">Self care</a>
+  <a href="<?=CYBSSO_URL?>/self/">Self care</a>
